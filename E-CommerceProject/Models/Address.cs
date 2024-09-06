aeba1c877caa134ee187db94ab49bae280e813a6
Mohamed-Ibrahim-89 <83844977+Mohamed-Ibrahim-89@users.noreply.github.com>
2024-09-06 23:46:12 +0300

    Add User & Address models

---
+++ E-CommerceProject/Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceProject.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        [MaxLength(30)]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [MaxLength(200)]
+        public string AddressLine1 { get; set; } = string.Empty;
+        [MaxLength(200)]
+        public string? AddressLine2 { get; set; }
+        [MaxLength(50)]
+        public string Country { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string City { get; set; } = string.Empty;
+        [MaxLength(100)]
+        public string? Landmark { get; set; }
+        public string? ZipCode { get; set; }
+        [MaxLength(50)]
+        public string? State { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; } = default!;
+    }
+}
