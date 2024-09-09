c8a2af8e989186626903cc6bf7f7cf3b9f42e5f2
AhmedEbrahim2322004 <ahmed2322004ahmed@gmail.com>
2024-09-09 19:09:45 +0300

    adding Payment & Shipment models

---
+++ E-CommerceProject.Entities/Models/Payment.cs
@@ -0,0 +1,9 @@
+﻿namespace E_CommerceProject.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public string PhoneNumber { get; set; }
+        
+    }
+}
