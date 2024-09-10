5d6e033d4ff30dff7724f11ce797b49a4fe7d5ee
ahmedmedhat2003 <ahmed.medhat.yousef@gmail.com>
2024-09-11 00:15:56 +0300

    Cart_wishlist&Discount

---
+++ E-CommerceProject.Entities/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceProject.Entities.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public int amount { get; set; }
+
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? DeletedAt { get; set;} 
+    }
+}
