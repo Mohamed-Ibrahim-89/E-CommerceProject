381b9095d5c6bff57d7a5578670fd88837af2dca
AhmedEbrahim2322004 <ahmed2322004ahmed@gmail.com>
2024-09-10 23:42:20 +0300

    adding payment & shipment models

--- E-CommerceProject.Entities/Models/Payment.cs
+++ E-CommerceProject.Entities/Models/Payment.cs
@@ -1,9 +1,19 @@
-﻿namespace E_CommerceProject.Models
+﻿
+using E_CommerceProject.Entities.Models;
+
+namespace E_CommerceProject.Models
 {
     public class Payment
     {
         public int PaymentId { get; set; }
-        public string PhoneNumber { get; set; }
-        
+        public string PaymentMethod { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public int PaymentAmount { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        //public int OrderId { get; set; }
+        //public Order order { get; set; } = default!;
     }
 }
--- E-CommerceProject.Entities/Models/Shipment.cs
+++ E-CommerceProject.Entities/Models/Shipment.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace E_CommerceProject.Entities.Models
 {
-    internal class Class1
+    internal class Shipment
     {
+        public int ShipmentId { get; set; }
+        public string Carrieer { get; set; }
+        public string TrackingNumber { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime EstimatedDeliveryDate { get; set; }
+        public int Cost { get; set; }
+
+        //public int OrderId { get; set; }
+        //public Order order { get; set; } = default!;
     }
 }
