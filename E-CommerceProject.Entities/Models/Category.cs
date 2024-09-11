f0e6d4b9b5fef7415c213ce3568c39bd9d8ef5fa
Ebrahim Khalil <125635563+Ebrahim1Khalil@users.noreply.github.com>
2024-09-11 15:20:57 +0300

    Add Product&Category Models

---
+++ E-CommerceProject.Entities/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceProject.Entities.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? DeleatedAt { get; set; }
+
+    }
+}
---
+++ E-CommerceProject.Entities/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceProject.Entities.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(700)]
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Cover { get; set; } = string.Empty;
+        public int QuantityInStock { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime DeleatedAt { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int DiscountId { get; set; }
+
+        public Discount? Discount { get; set; }
+
+    }
+}
