24e647f4257f627d3d3165ec7934b1a9f8056d39
Mistermostafa-cs <120172614+Mistermostafa-cs@users.noreply.github.com>
2024-10-05 19:38:43 +0300

    OrderController

---
+++ E-CommerceProject/Controllers/OrderController.cs
@@ -0,0 +1,123 @@
+﻿using E_CommerceProject.Entities.Models;
+using E_CommerceProject.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: OrderController
+        private IBaseRepository<Order> _order;
+        public OrderController(IBaseRepository<Order> order)
+        {
+            _order = order;
+        }
+
+        public async Task< ActionResult> Index()
+        {
+            var orders =await _order.GetAll();
+            return View(orders);
+        }
+
+        // GET: OrderController/Details/5
+        public async Task< ActionResult >Details(int id)
+        {
+            var order =await _order.GetById(id);
+            if (order == null)
+            { 
+                   return NotFound();
+            }
+            return View(order);
+        }
+
+        // GET: OrderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task< ActionResult> Create(Order order)
+        {
+                if (ModelState.IsValid)
+                {
+                try
+                {
+                    var orderCreate =await _order.AddItem(order);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    return View("Create");
+                }
+                }
+                return View("Create");
+        }
+
+        // GET: OrderController/Edit/5
+        public async Task< ActionResult> Edit(int id)
+        {
+            var order= await _order.GetById(id);
+            if (order==null) 
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: OrderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Order order)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _order.UpdateItem(order);
+                }
+                catch
+                {
+                    return View(order);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: OrderController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var order = await _order.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: OrderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id )
+        {
+            try
+            {
+               
+
+                await _order.DeleteItem(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+             
+                    
+    }
+}
